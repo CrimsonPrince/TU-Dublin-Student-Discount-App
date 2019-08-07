069e1faeef950abe629375eff7153d838a31af18
CrimsonPrince <CrimsonPrince@protonmail.com>
2019-08-08 00:37:47 +0100

    Added Sidebar

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule, NgModuleFactoryLoader, NO_ERRORS_SCHEMA } from "@angular/core";
 import { NativeScriptModule } from "nativescript-angular/nativescript.module";
+import { NativeScriptUISideDrawerModule } from "nativescript-ui-sidedrawer/angular";
+
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
@@ -13,6 +15,7 @@ import { LandmarksService } from "./landmarks-service";
     ],
     imports: [
         NativeScriptModule,
+        NativeScriptUISideDrawerModule,
         AppRoutingModule
     ],
     declarations: [
