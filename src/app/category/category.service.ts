6fb6b7fe3755b585980bc44414bb18c72d7972e2
crimsonprince <kingr4@protonmail.ch>
2019-09-15 22:44:15 +0100

    Got Image Overlay working. fixed padding

--- src/app/category/category.service.ts
+++ src/app/category/category.service.ts
@@ -16,14 +16,49 @@ export class CategoryService {
       discounts: [
         {
           id: '1',
-          name: 'Diceys',
+          name: 'Pablo Picante',
+          details: '4.99 Off Peak Burrito\'s in Aston Quay after 2pm',
+          imageUrl: '/assets/images/diceys.jpg',
+          location: 'Aston Quay'
+        },
+        {
+          id: '2',
+          name: 'Captain Americas',
           details: 'Lorem',
           imageUrl: 'he',
           location: 'dublin'
         },
         {
-          id: '2',
-          name: 'Academy',
+          id: '3',
+          name: 'Wowburger',
+          details: 'Lorem',
+          imageUrl: 'he',
+          location: 'dublin'
+        },
+        {
+          id: '4',
+          name: 'Bison Bar and BBQ',
+          details: 'Lorem',
+          imageUrl: 'he',
+          location: 'dublin'
+        },
+        {
+          id: '5',
+          name: 'Wagamamas',
+          details: 'Lorem',
+          imageUrl: 'he',
+          location: 'dublin'
+        },
+        {
+          id: '6',
+          name: 'Breakfest Me',
+          details: 'Lorem',
+          imageUrl: 'he',
+          location: 'dublin'
+        },
+        {
+          id: '7',
+          name: 'Leon Temple Bar',
           details: 'Lorem',
           imageUrl: 'he',
           location: 'dublin'
@@ -34,8 +69,88 @@ export class CategoryService {
       id: '2',
       name: 'Night',
       imageUrl: '../../assets/images/academy.jpg',
-      discounts: []
+      discounts: [
+        {
+          id: '1',
+          name: 'EverLeigh',
+          details: 'Lorem',
+          imageUrl: '/assets/images/everleigh.jpg',
+          location: 'dublin'
+        },
+        {
+          id: '2',
+          name: 'Workmans',
+          details: 'Lorem',
+          imageUrl: '/assets/images/workmans.jpg',
+          location: 'dublin'
+        },
+        {
+          id: '3',
+          name: 'Woolshed',
+          details: 'Lorem',
+          imageUrl: '/assets/images/tramline.jpg',
+          location: 'dublin'
+        },
+        {
+          id: '4',
+          name: 'Academy',
+          details: 'Lorem',
+          imageUrl: '/assets/images/academy.jpg',
+          location: 'dublin'
+        },
+        {
+          id: '5',
+          name: 'The Backpage',
+          details: 'Lorem',
+          imageUrl: '/assets/images/wowburger.jpg',
+          location: 'dublin'
+        }
+
+      ]
+    },
+    {
+      id: '3',
+      name: 'Shopping',
+      imageUrl: '../../assets/images/academy.jpg',
+      discounts: [
+        {
+          id: '1',
+          name: 'Tower Records',
+          details: 'Lorem',
+          imageUrl: 'he',
+          location: 'dublin'
+        },
+        {
+          id: '2',
+          name: 'Muji',
+          details: 'Lorem',
+          imageUrl: 'he',
+          location: 'dublin'
+        },
+        {
+          id: '3',
+          name: 'Aungier Print',
+          details: 'Lorem',
+          imageUrl: 'he',
+          location: 'dublin'
+        },
+        {
+          id: '4',
+          name: 'Savoy Cinema',
+          details: 'Lorem',
+          imageUrl: 'he',
+          location: 'dublin'
+        },
+        {
+          id: '5',
+          name: 'Grafton barbers',
+          details: 'Lorem',
+          imageUrl: 'he',
+          location: 'dublin'
+        }
+      ]
     }
+
   ];
 
 
